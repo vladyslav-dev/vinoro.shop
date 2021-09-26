9781b0c573f5e48f3bb20d1a853a5357656f4be8
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2021-09-26 23:58:28 +0300

    Made enum for header menu

--- client/components/NavbarMenu/index.tsx
+++ client/components/NavbarMenu/index.tsx
@@ -6,16 +6,42 @@ export interface NavbarMenuProps {
     category: Array<ICategory>
 };
 
-const NavbarMenu: FC<NavbarMenuProps> = ({ category }) => (
-    <>
-        {category && (
-            <div className={styles.navbarMenu}>
-                {category.map(item => (
+enum Catalog {
+    "Продукты питания" = 0,
+    "Алкогольные напитки" = 1,
+    "Бытовая химия" = 2,
+}
+
+const NavbarMenu: FC<NavbarMenuProps> = ({ category }) => {
+
+    const getCatalog = (key: string) => {
+        return category.map(item => {
+            if (Catalog[key] === item.catalog) {
+                return (
                     <div key={item._id}>{item.category_name}</div>
-                ))}
-            </div>
-        )}
-    </>
-);
+                )
+            }
+        })
+    }
+
+    return (
+        <>
+            {category && (
+                <div className={styles.navbarMenu}>
+                    {Object.values(Catalog).map((item, key) => {
+                        return typeof item === "string" && (
+                            <>
+                                <div key={key}>{item}</div>
+                                <div>
+                                    {getCatalog(item)}
+                                </div>
+                            </>
+                        )
+                    })}
+                </div>
+            )}
+        </>
+    );
+}
 
 export default NavbarMenu;

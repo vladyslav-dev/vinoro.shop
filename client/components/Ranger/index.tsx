5af58d8aa65030cd59bd6685f97b6b72ba31c226
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2021-10-06 20:05:00 +0300

    Added 2k container, installed react-ranger

--- client/components/Ranger/index.tsx
+++ client/components/Ranger/index.tsx
@@ -1,1 +1,52 @@
 import React from 'react';
+import styles from './Ranger.module.scss'
+import { useRanger } from "react-ranger";
+
+const Ranger = () => {
+
+    const [values, setValues] = React.useState([4]);
+
+    const { getTrackProps, handles } = useRanger({
+        min: 3,
+        max: 6,
+        stepSize: 1,
+        values,
+        onChange: setValues
+    });
+
+    console.log(values)
+
+    return (
+        <div className={styles.rangerWrapper}>
+            <div
+                {...getTrackProps({
+                    style: {
+                        height: "4px",
+                        background: "#BBBBBB",
+                        borderRadius: "4px"
+                    }
+                })}
+            >
+                {handles.map(({ getHandleProps }) => (
+                    <button
+                        {...getHandleProps({
+                            style: {
+                                cursor: "pointer",
+                                width: "20px",
+                                height: "20px",
+                                outline: "none",
+                                borderRadius: "100%",
+                                background: "#E8E8E8",
+                                border: "none",
+                                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                            }
+                        })}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+
+export default Ranger;
--- client/pages/category/[id].tsx
+++ client/pages/category/[id].tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import axios from 'axios';
+import styles from './category.module.scss';
 import CardList from '@/components/CardList';
-import CardFilter from '../../components/CardFilter/index'
+import CardFilter from '@/components/CardFilter'
+import Ranger from '@/components/Ranger';
 
 const Category = ({ products }) => {
     return (
-        <>
-            <CardFilter />
-            <CardList products={products} />
-        </>
+        <div className={styles["container-xl"]}>
+            <div className={styles.categoryWrapper}>
+                <div className={styles.categoryTree}>
+                    Component Tree
+                </div>
+                <div className={styles.categoryContent}>
+                    <div className={styles.categoryToolbar}>
+                        <CardFilter />
+                        <Ranger />
+                    </div>
+                    <div className={styles.categoryList}>
+                        <CardList products={products} customStyles={{}} />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 

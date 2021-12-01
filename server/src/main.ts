0669bb3aec09ee71452b86da97ebba7f32a7180c
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2021-12-01 23:01:23 +0200

    removed client and server folders

--- server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function start() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
-}
-start();

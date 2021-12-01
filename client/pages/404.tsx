4fb6c7402dea4ef01aac2377dfb2fea030c80049
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2021-12-01 21:29:55 +0200

    Made some fixes

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Router from 'next/router';
 
 export default function Custom404() {
-    React.useEffect(() => Router.push("/"), [])
+    React.useEffect(() => { Router.push("/") }, [])
 
-    return null
+    return <div></div>
 }

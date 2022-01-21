9085f32222f8ac590ee80fc1a2fadbc688503774
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2022-01-21 15:39:45 +0200

    Added toolbar translation

--- pages/api/category/[id].ts
+++ pages/api/category/[id].ts
@@ -2,26 +2,42 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '@/utils/dbConnect';
 import ProductModel from '@/models/ProductModel';
 import CategoryModel from '@/models/CategoryModel';
+// import { translateArrayOfProducts } from '@/utils/translatte';
 
 dbConnect();
 
 const api = async (req: NextApiRequest, res: NextApiResponse) => {
 
     try {
+
+        const { id, lang } = req?.query;
+
         const categoryModel = await CategoryModel
-            .findById(req.query.id).exec()
+            .findById(id).exec()
 
         if (categoryModel) {
             const category = await CategoryModel
                 .find({ catalog: categoryModel.catalog }).sort({ _id: 1 })
 
             const products = await ProductModel
-                .find({ category: req.query.id })
+                .find({ category: id })
                 .sort([['availability', -1], ['order_id', 0]]);
 
-            return res.status(200).json({ success: true, products, category });
+         
+
+            // const r = translateArrayOfProducts(products, "uk")
+            // console.log(r)
+            return res.status(200).json({ products, category });
+            // switch(lang) {
+            //     case "uk-UA": return translateArrayOfProducts(product, "uk")
+            //                     .then(p => res.status(200).json({ product: p, category: {category_name, _id} }))
+            //     case "en-US": return translateProduct(product, "en")
+            //                     .then(p => res.status(200).json({ product: p, category: {category_name, _id} }))
+            //     default: return res.status(200).json({ product, category: {category_name, _id}})
+            // }
+
         } else {
-            return res.status(200).json({ success: false, products: null, category: null });
+            return res.status(200).json({ products: null, category: null });
         }
 
     } catch (err) {

8091550954a3c2a52d9077f549827bf0750197fa
Vladyslav <51026693+vladyslav-dev@users.noreply.github.com>
2022-05-17 00:35:49 +0300

    changed styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,6 +41,14 @@ const Index = () => {
                 />
             </Head>
             <Fullpage />
+            <style jsx>{`
+                body {
+                    position: fixed;
+                    inset: 0;
+                    width: 100vw;
+                    height: 100vh;
+                }
+            `}</style>
             <style global jsx>{`
                 #nprogress .bar {
                     background: #fff;

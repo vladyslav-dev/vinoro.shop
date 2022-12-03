7ab56da4db1b7502c87fc3913abbf146c1fc0abb
ykorsiko <ykorsiko@cisco.com>
2022-12-03 16:41:09 +0200

    Build fix. Changed next-config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-const withPWA = require('next-pwa')
+/** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = withPWA({
+const nextConfig = withPWA({
   reactStrictMode: true,
   optimizeFonts: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
   devIndicators: {
     buildActivity: false,
   },
   ...nextTranslate(),
-})
+});
+module.exports = nextConfig;
